3e0d410fb64dc3ed7e26946a22f1252b141f554b
Emma Yeroshek <emma.yeroshek@gmail.com>
2019-11-04 19:45:23 +0200

    Added defalt sender email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "boilerplate_rails_api_production"
 
   config.action_mailer.perform_caching = false
+  config.default_sender_email = 'no-reply@localhost'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
