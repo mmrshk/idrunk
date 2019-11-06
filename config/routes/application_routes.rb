a8309d31b9b1e7fdddec90d9f8b1b0c7b8962b84
Emma Yeroshek <emma.yeroshek@gmail.com>
2019-11-06 09:13:43 +0200

    Fixed registration

--- config/routes/application_routes.rb
+++
@@ -1,9 +0,0 @@
-module ApplicationRoutes
-  def self.extended(router)
-    router.instance_exec do
-      scope module: :application do
-        root to: 'home#index'
-      end
-    end
-  end
-end
